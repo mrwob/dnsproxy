22414102cc8769d0907f9e15e7e5b9c196734d52
bowtonos <iamwob@gmail.com>
2014-07-13 11:33:53 +0800

    fixing up the config include bit

--- dnsProxy.js
+++ dnsProxy.js
@@ -1,24 +1,20 @@
-//node dnsProxy.js [conf]
+//node dnsProxy.js
+//edit conf.js to change settings.
 
 var debug = false;
 
 if(debug) var util = require('util')
 			 
+//yes the config is a module because i'm lazy...
 var dns = require('native-dns')
    ,async  = require('async')
    ,server = dns.createServer()
-   ,config = { ip: '0.0.0.0', port: 53, rdns: '203.0.178.191', override: {} }
- 
-//the config is a node module because i'm lazy. 
-if(process.argv[2]) {
- ['ip', 'port', 'rdns', 'override'].forEach(function(el) {
-   config[el] = require('./' + process.argv[2])[el] || config[el]
- })
-}
+   ,config = require('./conf.js');
    
+//this will probably break if a complicated query arrived with multiple questions but i don't think that happens very often.
 server.on('request', function(req, res) {
   res.id = req.id
-  async.each(req.question,function(q,cb) {
+  async.each(req.question, function(q,cb) {
     if(config.override[q.name]) {
       res.header.aa = 1
       res.answer.push(dns.A({
@@ -29,7 +25,7 @@ server.on('request', function(req, res) {
       return cb();
     }
     dns.Request({question: q, server: { address: config.rdns, type: 'udp' }})
-    .on('message',function(err, ans) {
+    .on('message', function(err, ans) {
       if(ans.header.rcode !== 0) res.header.rcode = ans.header.rcode;
       ['answer', 'authority', 'additional'].forEach(function(type) {
         ans[type].forEach(function(ret) {
